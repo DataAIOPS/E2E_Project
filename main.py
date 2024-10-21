2e0882ee49465ba9d804fadfe2d46de9c9c1165f
DataAIOPS <shubhkalpatil122@gmail.com>
2024-10-21 20:16:59 +0530

    Data_Cleaning phase is completed

---
+++ main.py
@@ -0,0 +1,8 @@
+import argparse
+import mlflow
+import os
+import time
+
+def main():
+    print("[INFO] MLOps Pipeline Triggerd")
+    
---
+++ src/Data_Cleaning.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import argparse
+import os
+import yaml
+
+with open("./config/config.yaml","r") as file:
+    config =  yaml.safe_load(file)
+
+raw_data_path = config['data_paths']['raw_data_path']
+clean_data_path = config['data_paths']['clean_data_path']
+raw_data_file = config['data_paths']['raw_data_file']
+
+
+def data_cleaning(raw_data_path, clean_data_path, raw_data_file):
+    print("################DATA CLEANING STARTED##################")
+    raw_data = os.path.join(raw_data_path,raw_data_file)
+    df = pd.read_csv(raw_data)
+    
+    '''
+    You can write some data cleaning steps here and your cleaned data will be exported
+
+    '''
+
+    clean_data_file = os.path.join(clean_data_path,raw_data_file)
+    df.to_csv(clean_data_file,index=False)
+    print("################DATA CLEANING FINISHED##################")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw_data_path",help="provide raw data path", default=raw_data_path)
+    parser.add_argument("--clean_data_path",help="provide cleaned data path", default=clean_data_path)
+    parser.add_argument("--raw_data_file",help="provide raw_data_file", default=raw_data_file)
+    args = parser.parse_args()
+    data_cleaning(args.raw_data_path,args.clean_data_path,args.raw_data_file)
+
+
+
